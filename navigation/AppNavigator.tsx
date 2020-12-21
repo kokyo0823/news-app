ae227fabe77f580eac6e591dfd8ed304e0b21ee6
kokyo0823 <kokyo0823@icloud.com>
2020-12-21 18:26:33 +0900

    ✨Add : tabnavigator

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -3,13 +3,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen';
+import ClipScreen from '../screens/ClipScreen';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const AppNavigator:React.FC = () => {
+const HomeStack = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <Stack.Navigator>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -19,6 +21,23 @@ const AppNavigator:React.FC = () => {
           component={ArticleScreen}
         />
       </Stack.Navigator>
+  )
+}
+const ClipStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Clip" component={ClipScreen}/>
+    </Stack.Navigator>
+  );
+};
+
+const AppNavigator:React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Clip" component={ClipStack} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
---
+++ screens/ClipScreen.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, SafeAreaView, Text } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+const ClipScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Clip</Text>
+    </SafeAreaView>
+  );
+};
+export default ClipScreen;
